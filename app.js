bf8d750f5db83dfd4243928186c6daddc1d415ae
Beast651 <arifsiddqui400@gmail.com>
2024-05-15 22:32:45 +0530

    Creating a website sort of similar like airbnb till now I am done with listing of houses as well as working on validation schema + added review and comment section.

---
+++ app.js
@@ -0,0 +1,227 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const Listing = require("./model/listing");
+const path = require("path");
+const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
+const wrapAsync = require("./utils/wrapAsync");
+const ExpressError = require("./utils/ExpressError");
+const { listingSchema, reviewSchema } = require("./schema");
+const Review = require("./model/review");
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, "/public")));
+
+main()
+  .then(() => {
+    console.log("Connected to DB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+async function main() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/wanderlust");
+}
+
+const validateListing = (req, res, next) => {
+  let { error } = listingSchema.validate(req.body);
+  console.log(error);
+
+  if (error) {
+    let errMsg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(400, errMsg);
+  } else {
+    next();
+  }
+};
+
+const validateReview = (req, res, next) => {
+  let { error } = reviewSchema.validate(req.body);
+  console.log(error);
+
+  if (error) {
+    let errMsg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(400, errMsg);
+  } else {
+    next();
+  }
+};
+
+app.get("/", (req, res) => {
+  res.send("HI I AM ROOT");
+});
+
+// Index Route
+
+app.get(
+  "/listings",
+  wrapAsync(async (req, res) => {
+    let allListings = await Listing.find();
+    res.render("./listings/index.ejs", { allListings });
+  })
+);
+
+// New Route
+
+app.get("/listings/new", (req, res) => {
+  res.render("listings/new.ejs");
+});
+
+// Create Route
+
+app.post(
+  "/listings",
+  validateListing,
+  wrapAsync(async (req, res, next) => {
+    // let { title, description, image, price, location, country } = req.body;
+
+    // let dataAdd = await new Listing({
+    //   title: title,
+    //   description: description,
+    //   image: image,
+    //   price: price,
+    //   location: location,
+    //   country: country,
+    // });
+
+    const dataAdd = new Listing(req.body.listing);
+
+    await dataAdd.save();
+
+    res.redirect("/listings");
+  })
+);
+
+// Edit Route
+
+app.get(
+  "/listings/:id/edit",
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    let editableContent = await Listing.findById(id);
+
+    res.render("listings/edit.ejs", { editableContent });
+  })
+);
+
+// Delete Route
+
+app.delete(
+  "/listings/:id",
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
+
+    let deletedProp = await Listing.findByIdAndDelete(id);
+    console.log(deletedProp);
+    res.redirect("/listings");
+  })
+);
+
+// Show Route
+
+app.get(
+  "/listings/:id",
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    const details = await Listing.findById(id).populate("reviews");
+    res.render("listings/show.ejs", { details });
+  })
+);
+
+// Update Route
+
+app.put(
+  "/listings/:id",
+  validateListing,
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    // let { title, description, image, price, location, country } = req.body;
+    // let updatedDesc = await Listing.findByIdAndUpdate(
+    //   id,
+    //   {
+    //     title: title,
+    //     description: description,
+    //     image: image,
+    //     price: price,
+    //     location: location,
+    //     country: country,
+    //   },
+    //   { runValidators: true, new: true }
+    // );
+    // console.log(updatedDesc);
+
+    await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+    res.redirect(`/listings/${id}`);
+  })
+);
+
+// Reviews
+// Post Review Route
+
+app.post(
+  "/listings/:id/reviews",
+  validateReview,
+  wrapAsync(async (req, res) => {
+    let listing = await Listing.findById(req.params.id);
+    let newReview = new Review(req.body.review);
+
+    listing.reviews.push(newReview);
+
+    await newReview.save();
+    await listing.save();
+
+    // console.log("New Review Saved");
+    // res.send("New Review Saved");
+
+    res.redirect(`/listings/${listing._id}`);
+  })
+);
+
+// Delete Review Route
+
+app.delete(
+  "/listings/:id/reviews/:reviewId",
+  wrapAsync(async (req, res) => {
+    let { id, reviewId } = req.params;
+
+    await Listing.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+    await Review.findByIdAndDelete(reviewId);
+
+    res.redirect(`/listings/${id}`);
+  })
+);
+
+// app.get("/testListing", async (req, res) => {
+//   let sampleTesting = new Listing({
+//     title: "My New Villa",
+//     description: "By the Beach",
+//     price: 1200,
+//     location: "Noida , UP",
+//     country: "India",
+//   });
+
+//   await sampleTesting.save().then((res) => {
+//     console.log(res);
+//   });
+//   res.send("Successful");
+// });
+
+app.all("*", (req, res, next) => {
+  next(new ExpressError(404, "Page Not Found!"));
+});
+
+app.use((err, req, res, next) => {
+  let { statusCode = 500, message = "Something Went Wrong!" } = err;
+  res.status(statusCode).render("error.ejs", { message });
+  // res.status(statusCode).send(message);
+});
+
+app.listen(8080, () => {
+  console.log("Server is listening at port 8080");
+});
